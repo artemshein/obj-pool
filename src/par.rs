2895045135147a5771d32b04768d9d6c18ec21ed
Artem Shein <artem.shein@esrlabs.com>
2021-11-08 13:09:30 +0100

    Make clear use ref

--- src/par.rs
+++ src/par.rs
@@ -53,7 +53,7 @@ impl<T, const S: usize> ParObjPool<T, S> {
         .ok()
     }
 
-    pub fn clear(&mut self) {
+    pub fn clear(&self) {
         for shard in self.shards {
             shard.write().clear();
         }
